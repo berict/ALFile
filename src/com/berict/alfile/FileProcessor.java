9165bbb21a46b334ee2dc1a4e84f19a4a663b233
BedrockDev <dev.bedrock@gmail.com>
2017-11-04 18:13:36 +0900

    Added template methods

---
+++ src/com/berict/alfile/FileProcessor.java
@@ -0,0 +1,12 @@
+package com.berict.alfile;
+
+public class FileProcessor {
+
+    public void move(FileWrapper fileWrapper) {
+
+    }
+
+    public void rename(FileWrapper fileWrapper) {
+
+    }
+}
