cf9fd8455922db32a8701889725a36cc86b3729f
BedrockDev <dev.bedrock@gmail.com>
2017-11-04 15:26:19 +0900

    Cleared project

--- out/src/Runner.java
+++
@@ -1,9 +0,0 @@
-import controller.AlFileController;
-import view.AlFile;
-
-public class Runner {
-    public static void main(String[] args) {
-        AlFileController controller = new AlFileController();
-        controller.showWindow();
-    }
-}
