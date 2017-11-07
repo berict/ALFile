bff64b3e5a774b321b417b53d3728383d61e5391
BedrockDev <dev.bedrock@gmail.com>
2017-11-08 00:44:58 +0900

    Refactored IDEA project to eclipse (also available on IDEA now)

---
+++ src/berict/alfile/Main.java
@@ -0,0 +1,15 @@
+package berict.alfile;
+
+import berict.alfile.main.MainController;
+
+public class Main {
+
+    public static void main(String[] args) {
+        init();
+    }
+
+    static void init() {
+        MainController controller = new MainController();
+    }
+
+}
---
+++ src/berict/alfile/main/MainController.java
@@ -0,0 +1,160 @@
+package berict.alfile.main;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.io.File;
+
+import javax.swing.ButtonGroup;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.SwingConstants;
+import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableCellRenderer;
+import javax.swing.table.TableColumnModel;
+
+public class MainController {
+
+    private JFrame frame = new JFrame();
+
+    public MainController() {
+
+        frame.setSize(new Dimension(960, 540));
+        // this kills the process on exit
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("AlFile");
+
+        ImageIcon img = new ImageIcon("icon.png");
+        frame.setIconImage(img.getImage());
+
+        initLeftPane();
+        initCenterPane();
+
+        frame.setVisible(true);
+    }
+
+    void initCenterPane() {
+        JPanel centerPane = new JPanel();
+        centerPane.setSize(740, frame.getHeight());
+
+        // get files
+        String path = "C://";
+        File dirFile = new File(path);
+        File[] fileList = dirFile.listFiles();
+
+        String[] columnNames = {
+                "Original File names",
+                "Changed File names",
+                "Location"
+        };
+
+        Object[][] data = new Object[fileList.length][3];
+
+        // set file table
+        int fileNum = 0;
+        for (int i = 0; i < fileList.length; i++) {
+            if (fileList[i].isFile()) {
+                data[fileNum][0] = fileList[i].getName();
+                data[fileNum][2] = fileList[i].getParent();
+                fileNum++;
+            }
+        }
+
+        DefaultTableModel tableModel = new DefaultTableModel(data, columnNames) {
+
+            @Override
+            public boolean isCellEditable(int row, int column) {
+                if (column == 1) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+
+        };
+
+        JTable table = new JTable();
+        table.setModel(tableModel);
+        table.setSize(500, 300);
+        table.setRowHeight(30);
+
+        // Center Align
+        DefaultTableCellRenderer defalutCellRenderer = new DefaultTableCellRenderer();
+        defalutCellRenderer.setHorizontalAlignment(SwingConstants.CENTER);
+        TableColumnModel cloumnModel = table.getColumnModel();
+        for (int i = 0; i < cloumnModel.getColumnCount(); i++)
+            cloumnModel.getColumn(i).setCellRenderer(defalutCellRenderer);
+
+        centerPane.add(table);
+        JScrollPane scrollPane = new JScrollPane(table);
+        scrollPane.setPreferredSize(new Dimension(740, 300));
+        centerPane.add(scrollPane);
+        frame.add(centerPane, BorderLayout.CENTER);
+    }
+
+    void initLeftPane() {
+        JPanel leftPane = new JPanel();
+        leftPane.setLayout(new GridLayout(11, 1));
+        leftPane.setBounds(0, 0, 220, frame.getHeight());
+        leftPane.setBackground(new Color(Integer.parseInt("B0BEC5", 16)));
+
+        // add items
+        String items[] = {
+                "Change file extension",
+                "To lower case",
+                "To upper case",
+                "Replace",
+                "Insert first",
+                "Insert end",
+                "Renumber"
+        };
+
+        addItemsToButton(items, leftPane);
+
+        // add radio buttons
+        JRadioButton r1 = new JRadioButton("All");
+        JRadioButton r2 = new JRadioButton("Selected");
+        r1.setBackground(new Color(Integer.parseInt("B0BEC5", 16)));
+        r2.setBackground(new Color(Integer.parseInt("B0BEC5", 16)));
+
+        r1.setSelected(true);
+
+        ButtonGroup bg = new ButtonGroup();
+        bg.add(r1);
+        bg.add(r2);
+
+        JPanel radioPane = new JPanel();
+        radioPane.setLayout(new GridLayout(2, 1));
+        radioPane.add(r1);
+        radioPane.add(r2);
+
+        // add margin
+        leftPane.add(new JLabel());
+        leftPane.add(new JLabel());
+
+        // add to layout
+        leftPane.add(radioPane);
+
+        // add process button
+        JButton process = new JButton("Process");
+        leftPane.add(process);
+
+        frame.setLayout(new BorderLayout());
+        frame.add(leftPane, BorderLayout.WEST);
+    }
+
+    void addItemsToButton(String items[], JPanel panel) {
+        for (String item : items) {
+            panel.add(new JButton(item));
+        }
+    }
+}
