9affa49695e9f9bcd2eb56ecd5d2bbfa127e61ef
Edwin-J <gkdhaos@naver.com>
2017-11-07 23:22:26 +0900

    Added function of load files

--- eclipse/src/berict/alfile/main/MainController.java
+++ eclipse/src/berict/alfile/main/MainController.java
@@ -2,8 +2,10 @@ package berict.alfile.main;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.GridLayout;
+import java.io.File;
 
 import javax.swing.ButtonGroup;
 import javax.swing.ImageIcon;
@@ -14,7 +16,11 @@ import javax.swing.JPanel;
 import javax.swing.JRadioButton;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.SwingConstants;
+import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableCellRenderer;
+import javax.swing.table.TableColumnModel;
 
 public class MainController {
 	
@@ -38,23 +44,31 @@ public class MainController {
 	
 	void initCenterPane() {
 		JPanel centerPane = new JPanel();
-//		centerPane.setLayout(new GridLayout(1, 3));
 		centerPane.setSize(740, frame.getHeight());
 		
-//		String[] originalFilenames = new String[99];
-//		
-//		JTable t1 = new JTable();
-//		JTableHeader header = new JT
-//		t1.setTableHeader(null);
+		// get files
+		String path = "C://";
+		File dirFile = new File(path);
+		File[] fileList = dirFile.listFiles();
 		
 		String[] columnNames = {
 				"Original File names",
 				"Changed File names",
 				"Location"
                 };
-
-		Object[][] data = new Object[99][3];
-
+		
+		Object[][] data = new Object[fileList.length][3];
+		
+		// set file table
+		int fileNum = 0;
+		for (int i = 0; i < fileList.length; i++) {
+			if (fileList[i].isFile()) {
+				data[fileNum][0] = fileList[i].getName();
+				data[fileNum][2] = fileList[i].getParent();
+				fileNum++;
+			}
+		}
+		
 		DefaultTableModel tableModel = new DefaultTableModel(data, columnNames) {
 			
 			@Override
@@ -70,9 +84,20 @@ public class MainController {
 		
 		JTable table = new JTable();
 		table.setModel(tableModel);
+		table.setSize(500, 300);
+		table.setRowHeight(30);
+		
+		// Center Align
+		DefaultTableCellRenderer defalutCellRenderer = new DefaultTableCellRenderer();
+		defalutCellRenderer.setHorizontalAlignment(SwingConstants.CENTER);
+		TableColumnModel cloumnModel = table.getColumnModel();
+		for (int i = 0; i < cloumnModel.getColumnCount(); i++)
+			cloumnModel.getColumn(i).setCellRenderer(defalutCellRenderer);
 		
 		centerPane.add(table);
-		centerPane.add(new JScrollPane(table));
+		JScrollPane scrollPane = new JScrollPane(table);
+		scrollPane.setPreferredSize(new Dimension(740, 300));
+		centerPane.add(scrollPane);
 		frame.add(centerPane, BorderLayout.CENTER);
 	}
 	
