fc792fe3bbbaa20bde3fba9168b4c164b1671f0d
BedrockDev <dev.bedrock@gmail.com>
2017-11-10 00:52:39 +0900

    Working on wrapper class

--- src/berict/alfile/file/File.java
+++ src/berict/alfile/file/File.java
@@ -4,7 +4,26 @@ import java.net.URI;
 
 public class File extends java.io.File {
 
-    String path;
+    /**
+     * The prefix concept is used to handle root directories on UNIX platforms, and drive specifiers, root directories and UNC pathnames on Microsoft Windows platforms, as follows:
+     *
+     * For UNIX platforms, the prefix of an absolute pathname is always "/". Relative pathnames have no prefix.
+     * The abstract pathname denoting the root directory has the prefix "/" and an empty name sequence.
+     *
+     * For Microsoft Windows platforms, the prefix of a pathname that contains a drive specifier consists of the drive letter followed by ":" and possibly followed by "\\" if the pathname is absolute.
+     * The prefix of a UNC pathname is "\\\\"; the hostname and the share name are the first two names in the name sequence.
+     * A relative pathname that does not specify a drive has no prefix.
+     *
+     * @link https://docs.oracle.com/javase/7/docs/api/java/io/File.html
+     */
+
+    public final static String SEPARATOR = pathSeparator;
+
+    // path : "some/dir/"
+    private String path;
+
+    // fileName : "file.extension"
+    private String fileName;
 
     // backup for the changed file objects
     private File original;
@@ -27,6 +46,20 @@ public class File extends java.io.File {
     public File(URI uri) {
         super(uri);
         original = this;
+    }
+
+    private void init(String absolutePath) {
+        // initializes the path and fileName values
+        int lastIndex = absolutePath.lastIndexOf(SEPARATOR);
+        path = absolutePath.substring(0, lastIndex);
+        fileName = absolutePath.substring(lastIndex + 1);
+        // TODO debug
+        System.out.println(toString());
+    }
+
+    public void revert() {
+        this.path = original.getAbsolutePath();
+        this.fileName = original.getName();
     }
 
     public void toLowerCase() {
@@ -53,11 +86,24 @@ public class File extends java.io.File {
 
     }
 
+    public String getPath() {
+        return path;
+    }
+
     public void setName(String name) {
 
     }
 
     public void apply() {
 
+    }
+
+    @Override
+    public String toString() {
+        return "File{" +
+                "path='" + path + '\'' +
+                ", fileName='" + fileName + '\'' +
+                ", original=" + original +
+                '}';
     }
 }
