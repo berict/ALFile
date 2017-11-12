cbf9ef7a9d364df172e8090bf5e4005f854ba029
BedrockDev <dev.bedrock@gmail.com>
2017-11-12 21:16:36 +0900

    Added all working example

--- src/berict/alfile/Main.java
+++ src/berict/alfile/Main.java
@@ -17,9 +17,45 @@ public class Main {
         // form method
         //MainFormController controller = new MainFormController();
 
-        File file = new File("C:\\Windows\\explorer.exe");
-        file.insertAtStart("parkmyoungchun-");
-        file.apply();
+        debug();
     }
 
+    private static void debug() {
+        File file = new File("C:\\Windows\\explorer.exe");
+        file.insertAtStart("ParkMyoungChun-");
+        file.apply();
+
+        file.toUpperCase();
+        file.apply();
+
+        file.toLowerCase();
+        file.apply();
+
+        file.replaceAll("parkmyoungchun", "LeeJuhoLee");
+        file.apply();
+
+        file.replaceFirst("Lee", "Kim");
+        file.apply();
+
+        file.replaceExtension("jpg");
+        file.apply();
+
+        file.insertAtEnd("vapor", false);
+        file.apply();
+
+        file.insertAtEnd("wave", true);
+        file.apply();
+
+        file.insertFileIndex(420, 2); //should make error
+        file.apply();
+
+        file.insertFileIndex(4, 3);
+        file.apply();
+
+        file.setName("asdf", false);
+        file.apply();
+
+        file.setName("internet-explorer.exe", true);
+        file.apply();
+    }
 }
