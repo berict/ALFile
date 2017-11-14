d05fc5371a2cde36f08df31e980d862465606c7f
BedrockDev <dev.bedrock@gmail.com>
2017-11-15 03:21:52 +0900

    Removed debug method

--- src/berict/alfile/Main.java
+++ src/berict/alfile/Main.java
@@ -18,48 +18,5 @@ public class Main {
 
         // form method
         MainFormController controller = new MainFormController();
-
-        if (DEBUG) {
-            debug();
-        }
-    }
-
-    private static void debug() {
-        File file = new File("C:\\Windows\\explorer.exe");
-        file.insertAtStart("ParkMyoungChun-");
-        file.apply();
-
-        file.toUpperCase();
-        file.apply();
-
-        file.toLowerCase();
-        file.apply();
-
-        file.replaceAll("parkmyoungchun", "LeeJuhoLee");
-        file.apply();
-
-        file.replaceFirst("Lee", "Kim");
-        file.apply();
-
-        file.replaceExtension("jpg");
-        file.apply();
-
-        file.insertAtEnd("vapor", false);
-        file.apply();
-
-        file.insertAtEnd("wave", true);
-        file.apply();
-
-        file.insertFileIndex(420, 2); //should make error
-        file.apply();
-
-        file.insertFileIndex(4, 3);
-        file.apply();
-
-        file.setName("asdf", false);
-        file.apply();
-
-        file.setName("internet-explorer.exe", true);
-        file.apply();
     }
 }
